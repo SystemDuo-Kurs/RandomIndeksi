1d0e8a1a15a27c4ffaba03e4f481bb8eb92b91d8
Dragan Pepic <d.pepic@hotmail.com>
2021-08-25 15:27:27 +0200

    Add project files.

---
+++ RandomIndeksi/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomIndeksi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> reci = new();
+            reci.Add("Test"); //0
+            reci.Add("Bla"); //1
+            reci.Add("Trece"); //2
+            reci.Add("Qwe"); //3
+            reci.Add("Asd"); //4
+
+            Random nasumicno = new();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine
+                    (
+                        reci[nasumicno.Next(reci.Count)]
+                    );
+            }
+
+            Console.WriteLine(reci[2]);
+            //for (int indeks = 0; indeks < reci.Count; indeks++)
+            //{
+            //    Console.WriteLine(reci[indeks]);
+            //}
+
+
+            //foreach(string rec in reci)
+            //{
+            //    Console.WriteLine(rec);
+            //}
+        }
+    }
+}
